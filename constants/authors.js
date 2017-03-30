19cb3be0ff8dd5bd9c13d35cce0a9691c3e984e8
Will Thirkettle <SirKettle@users.noreply.github.com>
2017-03-30 15:05:30 +0100

    Create authors.js

---
+++ constants/authors.js
@@ -0,0 +1,35 @@
+
+import willAvatarSrc from 'images/avatars/will.png';
+import vickyAvatarSrc from 'images/avatars/will.png';
+import harrisonAvatarSrc from 'images/avatars/will.png';
+import elliotAvatarSrc from 'images/avatars/will.png';
+
+export const AUTHORS = {
+  WILL: 'WILL',
+  VICKY: 'VICKY',
+  HARRISON: 'HARRISON',
+  ELLIOT: 'ELLIOT'
+};
+
+export const authorData = {
+  WILL: {
+    name: 'Will',
+    summary: 'Will (aka Dad) is the smelliest member of the family',
+    avatarSrc: willAvatarSrc
+  },
+  VICKY: {
+    name: 'Vicky',
+    summary: 'Vicky (aka Mum) is the prettiest member of the family',
+    avatarSrc: vickyAvatarSrc
+  },
+  HARRISON: {
+    name: 'Harrison',
+    summary: 'Harrison (aka Hassie) is cheeky and strong with the force',
+    avatarSrc: harrisonAvatarSrc
+  },
+  ELLIOT: {
+    name: 'Elliot',
+    summary: 'Elliot (aka Elly) is extremely cheeky and could turn to the dark side',
+    avatarSrc: elliotAvatarSrc
+  }
+};
